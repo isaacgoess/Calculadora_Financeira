2754142949682bc1ac89aae5043f1584a9bf8348
isaacgoess <36758680+isaacgoess@users.noreply.github.com>
2019-04-09 10:13:43 -0300

    Banco
    Corrigido

---
+++ CalculadoraFinanceira/Models/Classes/Receita.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraFinanceira.Models.Classes
+{
+    public class Receita
+    {
+        public int Id { get; set; }
+        public float Valor { get; set; }
+        public DateTime DataRecebimento { get; set; }
+        public String Descricao { get; set; }
+        public String Observacao { get; set; }
+        public bool Situacao { get; set; }
+        public MeioRecebimento FormaRecebimento { get; set; }
+        public ParcelaReceita Parcela { get; set; }
+
+        public int IdTipoReceita { get; set; }
+        public TipoReceita TipoReceita { get; set; }
+
+        public enum MeioRecebimento
+        {
+            Cheque = 1, Dinheiro, Cartao, Transferencia
+        }
+
+        public Receita()
+        {
+            this.Situacao = true;
+        }
+    }
+
+    public class ParcelaReceita
+    {
+        public TipoParcela FormaReceita { get; set; }
+
+        public enum TipoParcela
+        {
+            [Description("Única")]
+            Unica,
+            [Description("Parcelada")]
+            Parcelado
+        }
+
+        [DisplayName("Parcelas")]
+        public int NumeroParcelas { get; set; }
+
+    }
+}
---
+++ CalculadoraFinanceira/Models/Validator/ListarDespesaValidator.cs
@@ -0,0 +1,32 @@
+﻿using CalculadoraFinanceira.Models.Classes;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraFinanceira.Models.Validator
+{
+    public class ListarDespesaValidator : AbstractValidator<ListarDespesa>
+    {
+        CalculadoraFinanceiraContext db = null;
+
+        public ListarDespesaValidator()
+        {
+            this.db = new CalculadoraFinanceiraContext();
+            RuleFor(l => l.Nome).MaximumLength(255).WithMessage("Máximo de 255 caracteres");
+            RuleFor(l => l.Nome).Must(UniqueName).WithMessage("Tipo de Categoria de Despesa Cadastrada");
+        }
+
+
+        private bool UniqueName(String nome)
+        {
+            var result = this.db.ListarDespesas
+                                 .Where(x => x.Nome.ToLower() == nome.ToLower())
+                                 .Count();
+
+            return result == 0;
+        }
+
+    }
+}
